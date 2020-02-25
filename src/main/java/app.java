e2d7b7c000ba62214eb470060fcd78f09198718d
Frank Uzoka <Frankuzoka@hotmail.com>
2020-02-24 22:08:34 -0500

    Practice with random

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -1,6 +1,18 @@
+import java.util.Random;
+
 public class app {
 
     public static void main(String[] args) {
 
     }
+
+    public static void exampleForRandom(){
+
+        Random rand = new Random();
+
+       int randomNumber = rand.nextInt(10);
+
+
+    }
+
 }
